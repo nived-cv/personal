5ad52b0d709fa9d282b0e522d1284d54183f1fd4
nived-cv <nived.cv@vonnue.com>
2023-10-22 10:06:41 +0530

    stats counter app initial setup

--- automate/index.js
+++ automate/index.js
@@ -8,17 +8,17 @@ const getData = async()=>{
   await page.setViewport({width: 1080, height: 768});
   await page.goto('https://app.software.com/login')
   await page.click('.button_to')
-  // await page.waitForSelector('#login_field')
-  // await page.type('#login_field','nived-cv')
-  // await page.type('#password','Nicvnicv187')
-  // await page.click('.js-sign-in-button')
-  // await page.waitForSelector('.highcharts-series-1')
+  await page.waitForSelector('#login_field')
+  await page.type('#login_field','nived-cv')
+  await page.type('#password','Nicvnicv187')
+  await page.click('.js-sign-in-button')
+  await page.waitForSelector('.highcharts-series-1')
   
-  //   let text = document.querySelectorAll('.highcharts-point')
-  //   text.forEach(async(x)=>{
-  //     console.log('looping')
-  //     await page.click(x)
-  //   })
+    let text = document.querySelectorAll('.highcharts-point')
+    text.forEach(async(x)=>{
+      console.log('looping')
+      await page.click(x)
+    })
 
 }
 getData()
---
+++ automate/meow.sh
@@ -0,0 +1,21 @@
+
+#  a=`wc -l *.json`
+ 
+#  for x in $a
+#  do
+#  echo "$x \."
+#  done
+#cat *js | wc -l
+
+#b=`echo"a.{html,css,js}"`
+
+#rm a.{html,css,js} \
+
+#cat "index.{html,css,js}" | wc -l  #working
+
+#
+
+
+
+
+
