f34adfcffce0c0fe385a6a037b2ea52fac612842
nived-cv <nived.cv@vonnue.com>
2023-10-24 13:04:21 +0530

    1.0.2

--- automate/stats.sh
+++ automate/stats.sh
@@ -1,26 +1,28 @@
 
-current=$(pwd)
-lookup=$(grep $current 'lookup.csv' | wc -l)
 
-newHtmlLines=$(find . -name "*.html"  -exec cat {} +| grep . | wc -l)
-newCssLines=$(find . -name "*.css"  -exec cat {} +| grep . | wc -l)
-newJsLines=$(find . -name "*.js"  -exec cat {} +| grep . | wc -l)
+current=$(pwd)
+lookup=$(grep -l $current '/home/nived/bin/lookup.csv' | wc -l)
+echo $lookup
+
+newHtmlLines=$(find . -name "*.html"  -exec cat {} + | grep . | wc -l)
+newCssLines=$(find . -name "*.css"  -exec cat {} + | grep . | wc -l)
+newJsLines=$(find . -name "*.js"  -exec cat {} + | grep . | wc -l)
 
 if (( $lookup == 0))
 then
-echo "$current,$newHtmlLines,$newCssLines,$newJsLines" >> 'lookup.csv'
-echo "html" $newHtmlLines "css" $newCssLines "js" $newJsLines 
+echo "$current,$newHtmlLines,$newCssLines,$newJsLines" >> '/home/nived/bin/lookup.csv'
+echo -e "\n html  :" $newHtmlLines "\n css   :" $newCssLines "\n js    :" $newJsLines 
 else
-oldHtmlLines=$(awk -F, -v curr=$current '{ if(curr == $1) print $2}' 'lookup.csv')
-oldCssLines=$(awk -F, -v curr=$current '{ if(curr == $1) print $3}' 'lookup.csv')
-oldJsLines=$(awk -F, -v curr=$current '{ if(curr == $1) print $4}' 'lookup.csv')
+oldHtmlLines=$(awk -F, -v curr=$current '{ if(curr == $1) print $2}' '/home/nived/bin/lookup.csv')
+oldCssLines=$(awk -F, -v curr=$current '{ if(curr == $1) print $3}' '/home/nived/bin/lookup.csv')
+oldJsLines=$(awk -F, -v curr=$current '{ if(curr == $1) print $4}' '/home/nived/bin/lookup.csv')
 
 jsLines=$(($newJsLines - $oldJsLines))
 htmlLines=$(($newHtmlLines - $oldHtmlLines))
 cssLines=$(($newCssLines - $oldCssLines))
 
-sed -i "s|$current,$oldHtmlLines,$oldCssLines,$oldJsLines|$current,$newHtmlLines,$newCssLines,$newJsLines|" 'lookup.csv'
-echo -e "\n html  :" $htmlLines "\n css   :" $cssLines  "\n js    :" $jsLines 
+sed -i "s|$current,$oldHtmlLines,$oldCssLines,$oldJsLines|$current,$newHtmlLines,$newCssLines,$newJsLines|" '/home/nived/bin/lookup.csv'
+echo -e "\n html  :" $htmlLines "\n css   :" $cssLines  "\n js    :" $jsLines
 fi
 
 
