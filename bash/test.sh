783a7733ff6c2ca62461fc49dd6af7e46158b5c6
nived-cv <nived.cv@vonnue.com>
2023-12-24 10:22:57 +0530

    completed

---
+++ bash/test.sh
@@ -0,0 +1,1 @@
+git log --author="nived" > "test.txt"
---
+++ statsgo/test.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("hi")
+	fmt.Println(getname())
+}
+
+func getname() (double){
+	return 1
+}
