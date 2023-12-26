e80784b1e4e953ff505360588710bbc3a9e31ba8
nived-cv <nived.cv@vonnue.com>
2023-12-26 10:31:18 +0530

    just to push

--- statsgo/test.go
+++ statsgo/test.go
@@ -1,13 +1,37 @@
 package main
 import (
 	"fmt"
+	//"io/ioutil"
+	//"encoding/json"
 )
 
-func main(){
-	fmt.Println("hi")
-	fmt.Println(getname())
+type fileData struct{
+	name string
+	age int
 }
 
-func getname() (double){
-	return 1
+func main(){
+	//fmt.Println("hi")
+	// fmt.Println(getname())
+	readtoMem()
+}
+
+func getname() (byte){
+	return 102
+}
+
+func readtoMem(){
+	// data, err := ioutil.ReadFile("dummy.json")
+
+	// if err != nil {
+	// 	fmt.Println("file reading error", err)
+	// }
+	// var result fileData
+	// json.Unmarshal(data,&result)
+	// fmt.Println(result)
+
+	var user []fileData = { "name" : "akash", age: 10}
+	
+
+	fmt.Println(user)
 }
--- web/index.js
+++ web/index.js
@@ -8,7 +8,7 @@ console.log("helloooo")
 main()
 
 const func2 =() =>{
-	console.log("please say hi")
+	console.log("please say hi from")
 }
 
 func2()
