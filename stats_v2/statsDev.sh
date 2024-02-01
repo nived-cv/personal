2e2afa85774277b51cc0754a59c452a0de8e5a7e
nived-cv <nived.cv@vonnue.com>
2024-02-01 15:12:55 +0530

    working fine

--- stats_v2/statsDev.sh
+++ stats_v2/statsDev.sh
@@ -1,9 +1,8 @@
 # git branch >> branches.txt
 # git log --author='nived' >> commits.txt
 
-function get_commits() {
-
-   git branch >> branches.txt
+function get_branches() {
+   git branch > branches.txt
    sed -i "s|*||" branches.txt
    readarray a < branches.txt
     for letter in $a; do
@@ -11,4 +10,18 @@ function get_commits() {
     done
 }
 
-get_commits
+function get_changes() {
+    readarray branches < branches.txt
+    echo $branches
+    for branch in $branches; do 
+        git checkout $branch
+        echo 'hi'
+        git log --author='nived' > `${branch}.txt`
+    done
+}
+
+function main() {
+    get_changes
+}
+
+main
